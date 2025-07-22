ed052486c71a6e204beddcd60917eebd860dd856
Kamron5505 <kama58077@gmail.com>
2025-07-22 16:52:13 +0500

    hello game

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import img from "../../assets/joystick.png"
+import "../Header/header.css"
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+      <header>
+        <div className="container">
+          <div className="header-wrapper">
+            <div className="header-wrapper__content">
+              <a href="#" className="header__link">3D game Dev </a>
+              <h1 className="header__title">Work that we <br /> produce for our <br /> clients</h1>
+              <p className="header__description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard</p>
+              <button className="header__btn">Get more details</button>
+            </div>
+            <div className="header-wrapper__img">
+              <img src={img} alt="" />
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
   )
 }
 
