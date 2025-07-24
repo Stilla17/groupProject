e9af5a0b4a7b681f51111a1771df22dd1b7f6e45
Kamron5505 <kama58077@gmail.com>
2025-07-24 16:36:26 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from './Component/Header/Header'
+import footer from "./Component/Footer/Footer"
+import Footer from './Component/Footer/Footer'
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   )
 }
